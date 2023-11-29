5593fcc470d99686cc816cd4f62f4b1ee2a8110c
Isaiasiv <112592786+Isaiasiv@users.noreply.github.com>
2023-11-29 13:39:10 -0300

    versão 1
    imagens, links, paginas

--- menuCabecario.js
+++ menuCabecario.js
@@ -1,21 +1,34 @@
 // menu.js
+
+// Espera o carregamento completo do DOM antes de executar o código interno
 document.addEventListener("DOMContentLoaded", function () {
-    // Carregar o conteúdo do menu
+
+    // Obtém a referência ao elemento HTML com o ID "menuCabecario"
     var menuCabecario = document.getElementById("menuCabecario");
 
-    // Verificar se o contêiner do menu existe na página
+    // Verifica se o contêiner do menu existe na página
     if (menuCabecario) {
-        // Carregar o conteúdo do menu.html no contêiner do menu
+
+        // Cria um objeto XMLHttpRequest para fazer requisições HTTP assíncronas
         var xhr = new XMLHttpRequest();
+
+        // Abre uma conexão para obter o conteúdo do arquivo "menuCabecario.html"
         xhr.open("GET", "menuCabecario.html", true);
+
+        // Define uma função a ser chamada sempre que o estado da requisição mudar
         xhr.onreadystatechange = function () {
+            // Verifica se a requisição foi concluída e bem-sucedida
             if (xhr.readyState == 4 && xhr.status == 200) {
+
+                // Atualiza o conteúdo do elemento "menuCabecario" com o conteúdo obtido do arquivo HTML
                 menuCabecario.innerHTML = xhr.responseText;
 
-                // Destacar o item do menu correspondente à página atual
+                // Chama a função para destacar o item do menu correspondente à página atual
                 highlightCurrentPage();
             }
         };
+
+        // Envia a requisição para obter o conteúdo do arquivo HTML
         xhr.send();
     }
 });
